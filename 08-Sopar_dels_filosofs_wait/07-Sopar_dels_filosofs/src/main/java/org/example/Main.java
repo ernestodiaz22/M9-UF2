6912d5abd2b083b6b4994c0044a8fa2ca1543fc6
ernestodiaz22 <ernestodiaz22@ilg.cat>
2025-02-28 17:05:14 +0100

    Actividad 8: últimos retoques

--- 08-Sopar_dels_filosofs_wait/07-Sopar_dels_filosofs/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Taula taula = new Taula(4);
-        taula.showTaula();
-        taula.cridarTaula();
-    }
-}
